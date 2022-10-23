a9cc155ade66c49c7d6dea479ae4e81a78b0737c
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-23 17:29:32 -0300

    Correção feita.

--- td.js
+++ td.js
@@ -1,87 +1,142 @@
-class Tower {
-    #Name;
-    #Power;
-    #Range;
-    #Level;
-    #Price;
-    constructor(name, power, range, level, price) {
-        this.#Name = name;
-        this.#Power = power;
-        this.#Range = range;
-        this.#Level = level;
-        this.#Price = price;
+var Tower = /** @class */ (function () {
+    function Tower(name, power, range, level, price) {
+        this.setName(name);
+        this.setPower(power);
+        this.setRange(range);
+        this.setLevel(level);
+        this.setPrice(price);
     }
-    setName(name) {
-        this.#Name = name;
+    Tower.prototype.setName = function (name) {
+        this.Name = name;
+    };
+    Tower.prototype.getName = function () {
+        return this.Name;
+    };
+    Tower.prototype.setPower = function (power) {
+        this.Power = power;
+    };
+    Tower.prototype.getPower = function () {
+        return this.Power;
+    };
+    Tower.prototype.setRange = function (range) {
+        this.Range = range;
+    };
+    Tower.prototype.getRange = function () {
+        return this.Range;
+    };
+    Tower.prototype.setLevel = function (level) {
+        this.Level = level;
+    };
+    Tower.prototype.getLevel = function () {
+        return this.Level;
+    };
+    Tower.prototype.setPrice = function (price) {
+        this.Price = price;
+    };
+    Tower.prototype.getPrice = function () {
+        return this.Price;
+    };
+    return Tower;
+}());
+var Enemy = /** @class */ (function () {
+    function Enemy(name, HP) {
+        this.setName(name);
+        this.setHealthPoints(HP);
     }
-    getName() {
-        return this.#Name;
-    }
-    setPower(power) {
-        this.#Power = power;
-    }
-    getPower() {
-        return this.#Power;
-    }
-    setRange(range) {
-        this.#Range = range;
-    }
-    getRange() {
-        return this.#Range;
-    }
-    setLevel(level) {
-        this.#Level = level;
-    }
-    getLevel() {
-        return this.#Level;
-    }
-    setPrice(price) {
-        this.#Price = price;
-    }
-    getPrice() {
-        return this.#Price;
-    }
-    Attack() {
-        return this.#Power;
-    }
-}
-class Enemy {
-    #Name;
-    #HealthPoints;
-    constructor(name, HP) {
-        this.#Name = name;
-        this.#HealthPoints = HP;
-    }
-    setName(name) {
-        this.#Name = name;
-    }
-    getName() {
-        return this.#Name;
-    }
-    setHealthPoints(HP) {
-        this.#HealthPoints = HP;
-    }
-    getHealthPoints() {
-        return this.#HealthPoints;
-    }
-    TakeDamage(damage) {
-        this.setHealthPoints(this.getHealthPoints() - damage);
-        return;
-    }
-}
-const arrayTower = new Array(10);
-const arrayEnemy = new Array(10);
-let Tower1 = new Tower("A", 3, 1, 1, 5);
-let Enemy1 = new Enemy("Goblin", 12);
-PutTower(Tower1, 11);
-console.log(arrayTower);
+    Enemy.prototype.setName = function (name) {
+        this.Name = name;
+    };
+    Enemy.prototype.getName = function () {
+        return this.Name;
+    };
+    Enemy.prototype.setHealthPoints = function (HP) {
+        this.HealthPoints = HP;
+    };
+    Enemy.prototype.getHealthPoints = function () {
+        return this.HealthPoints;
+    };
+    Enemy.prototype.TakeDamage = function (damage) {
+        return this.HealthPoints = this.HealthPoints - damage;
+    };
+    return Enemy;
+}());
+var arrayTowerPlace = new Array(10);
+var arrayEnemyWay = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,];
+var arrayEnemy = [];
+var Tower1 = new Tower("A", 3, 2, 1, 5);
+var Enemy1 = new Enemy("Goblin", 100);
+PutTower(Tower1, 10);
+PutTower(Tower1, 3);
+StartGame(10);
 function PutTower(tower, position) {
     if (position < 1 || position > 10) {
         console.log("Número da casa inválida!");
         return;
     }
     else {
-        arrayTower[position - 1] = tower;
+        arrayTowerPlace[position - 1] = tower;
         return;
     }
+}
+function StartGame(totalMonsters) {
+    var playerHP = 10;
+    var defeatedMonsters = 0;
+    var numberMonsters = totalMonsters;
+    var gameOver = false;
+    do {
+        numberMonsters = numberMonsters - EnemyMoves(numberMonsters);
+        playerHP = playerHP - EnemyAttacks();
+        defeatedMonsters = defeatedMonsters + EnemyAttacks();
+        defeatedMonsters = defeatedMonsters + TowerAttacks();
+        console.log("Player HP - " + playerHP);
+        if (playerHP <= 0) {
+            gameOver = true;
+            console.log("YOU LOSE!");
+        }
+        else if (defeatedMonsters == totalMonsters) {
+            gameOver = true;
+            console.log("YOU WIN!");
+        }
+    } while (!gameOver);
+}
+function EnemyMoves(numberMonsters) {
+    arrayEnemyWay.shift();
+    if (numberMonsters != 0) {
+        arrayEnemyWay[9] = Enemy1;
+        return 1;
+    }
+    else {
+        arrayEnemyWay[9] = undefined;
+    }
+    return 0;
+}
+function EnemyAttacks() {
+    if (arrayEnemyWay[0] != undefined) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+function TowerAttacks() {
+    var defeatedMonsters = 0;
+    arrayTowerPlace.forEach(function (x, index) {
+        if (x.getRange() == 1 && arrayEnemyWay[index] != undefined) {
+            return defeatedMonsters = defeatedMonsters + VerifyKill(arrayEnemyWay[index].TakeDamage(x.getPower()), index);
+        }
+        else {
+            for (var i = (index - x.getRange() + 1); i <= (index + x.getRange() - 1); i++) {
+                if (arrayEnemyWay[i] != undefined)
+                    return defeatedMonsters = defeatedMonsters + VerifyKill(arrayEnemyWay[i].TakeDamage(x.getPower()), i);
+            }
+        }
+    });
+    return defeatedMonsters;
+}
+function VerifyKill(HP, position) {
+    if (HP <= 0) {
+        arrayEnemyWay[position] = undefined;
+        return 1;
+    }
+    return 0;
 }
--- td.ts
+++ td.ts
@@ -1,102 +1,165 @@
-class Tower{
-    #Name : string
-    #Power : number
-    #Range : number
-    #Level : number
-    #Price : number
 
-    constructor(name : string, power : number, range : number, level: number, price : number){
-        this.#Name = name
-        this.#Power = power
-        this.#Range = range
-        this.#Level = level
-        this.#Price = price
-    }
-    setName(name : string) : void{
-        this.#Name = name
-    }
-    getName() : string{
-        return this.#Name
-    }
 
-    setPower(power : number) : void{
-        this.#Power = power
+class Tower {
+    private Name: string
+    private Power: number
+    private Range: number
+    private Level: number
+    private Price: number
+
+    constructor(name: string, power: number, range: number, level: number, price: number) {
+        this.setName(name)
+        this.setPower(power)
+        this.setRange(range)
+        this.setLevel(level)
+        this.setPrice(price)
     }
-    getPower() : number{
-        return this.#Power
+    public setName(name: string): void {
+        this.Name = name
+    }
+    public getName(): string {
+        return this.Name
     }
 
-    setRange(range : number) : void{
-        this.#Range = range
+    public  setPower(power: number): void {
+        this.Power = power
     }
-    getRange() : number{
-        return this.#Range
-    }
-    
-    setLevel(level : number) : void{
-        this.#Level = level
-    }
-    getLevel() : number{
-        return this.#Level
+    public getPower(): number {
+        return this.Power
     }
 
-    setPrice(price : number) : void{
-        this.#Price = price
+    public setRange(range: number): void {
+        this.Range = range
     }
-    getPrice() : number{
-        return this.#Price
+    public getRange(): number {
+        return this.Range
     }
 
-    Attack() : number{
-        return this.#Power
+    public setLevel(level: number): void {
+        this.Level = level
+    }
+    public getLevel(): number {
+        return this.Level
+    }
+
+    public setPrice(price: number): void {
+        this.Price = price
+    }
+    public getPrice(): number {
+        return this.Price
     }
 }
 
-class Enemy{
-    #Name : string
-    #HealthPoints : number
+class Enemy {
+    private Name: string
+    private HealthPoints: number
 
-    constructor(name : string, HP : number){
-        this.#Name = name
-        this.#HealthPoints = HP
-    }
-
-    setName(name : string) : void{
-        this.#Name = name
-    }
-    getName() : string{
-        return this.#Name
+    constructor(name: string, HP: number) {
+        this.setName(name)
+        this.setHealthPoints(HP)
     }
 
-    setHealthPoints(HP : number) : void{
-        this.#HealthPoints = HP
+    public setName(name: string): void {
+        this.Name = name
     }
-    getHealthPoints() : number{
-        return this.#HealthPoints
+    public getName(): string {
+        return this.Name
     }
-    
-    TakeDamage(damage : number) : void{
-        this.setHealthPoints(this.getHealthPoints() - damage)
-        return
+
+    public setHealthPoints(HP: number): void {
+        this.HealthPoints = HP
+    }
+    public getHealthPoints(): number {
+        return this.HealthPoints
+    }
+
+    public TakeDamage(damage: number): number {
+        return this.HealthPoints = this.HealthPoints - damage
     }
 }
 
-const arrayTower : Tower[] = new Array(10)
 
-const arrayEnemy : Enemy[] = new Array(10)
+let arrayTowerPlace: Tower[] = new Array(10)
+let arrayEnemyWay: (Enemy | undefined)[] = [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,]
+let arrayEnemy: Enemy[] = []
 
-let Tower1 = new Tower("A",3,1,1,5)
-let Enemy1 = new Enemy("Goblin",12)
+let Tower1 = new Tower("A", 3, 2, 1, 5)
+let Enemy1 = new Enemy("Goblin", 100)
 
-PutTower(Tower1, 11)
-console.log(arrayTower)
+PutTower(Tower1,10)
+PutTower(Tower1,3)
 
-function PutTower(tower : Tower, position : number) : void{
-    if(position < 1 || position > 10){
+StartGame(10)
+
+function PutTower(tower: Tower, position: number): void {
+    if (position < 1 || position > 10) {
         console.log("Número da casa inválida!")
         return
-    }else{
-    arrayTower[position - 1] = tower
-    return
+    } else {
+        arrayTowerPlace[position - 1] = tower
+        return
     }
+}
+
+function StartGame(totalMonsters: number): void {
+    let playerHP: number = 10
+    let defeatedMonsters : number = 0
+    let numberMonsters : number = totalMonsters
+    let gameOver : boolean = false
+    do{
+        numberMonsters = numberMonsters - EnemyMoves(numberMonsters)
+        playerHP = playerHP - EnemyAttacks()
+        defeatedMonsters = defeatedMonsters + EnemyAttacks()
+        defeatedMonsters = defeatedMonsters + TowerAttacks()
+        console.log("Player HP - " +playerHP)
+        if(playerHP <= 0){
+            gameOver = true
+            console.log("YOU LOSE!")
+        }else if(defeatedMonsters == totalMonsters){
+            gameOver = true
+            console.log("YOU WIN!")
+        }
+    } while(!gameOver)
+}
+ 
+function EnemyMoves(numberMonsters : number): number {
+    arrayEnemyWay!.shift()
+    if(numberMonsters != 0){
+        arrayEnemyWay![9] = Enemy1
+        return 1
+    }else{
+        arrayEnemyWay[9] = undefined
+    }
+    return 0
+}
+
+function EnemyAttacks() : number{
+    if(arrayEnemyWay![0] != undefined){
+        return 1
+    }else{
+        return 0
+    }
+}
+
+function TowerAttacks() : number{
+    let defeatedMonsters : number = 0
+    arrayTowerPlace.forEach((x,index) => {
+            if(x.getRange() == 1 && arrayEnemyWay![index] != undefined){
+                return defeatedMonsters = defeatedMonsters + VerifyKill(arrayEnemyWay[index]!.TakeDamage(x.getPower()),index)
+            }else {
+                for(let i : number = (index - x.getRange() + 1); i <= (index + x.getRange() - 1); i++ ){
+                    if(arrayEnemyWay[i] != undefined)
+                    return defeatedMonsters = defeatedMonsters + VerifyKill(arrayEnemyWay[i]!.TakeDamage(x.getPower()), i)
+                }
+            }
+    })
+    return defeatedMonsters
+}
+
+function VerifyKill(HP : number, position : number) : number {
+    if(HP <= 0) {
+        arrayEnemyWay[position] = undefined
+        return 1
+    }
+    return 0
 }
